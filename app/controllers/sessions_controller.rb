b55fd8b751131bb073c9e327cbd175f0111f9b02
Akito Tsutsui <akito.tsutsui@gmail.com>
2019-07-23 07:08:26 +0900

    ログアウト機能を実装

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -13,6 +13,11 @@ class SessionsController < ApplicationController
     end
   end
 
+  def destroy
+    reset_session
+    redirect_to root_path, notice: 'Logout Succeeded'
+  end
+
   private
 
   def session_params
