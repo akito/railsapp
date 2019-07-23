9d2ddef073d4b1fe569a07031f7a126f43568b9e
Akito Tsutsui <akito.tsutsui@gmail.com>
2019-07-23 10:08:50 +0900

    Checkout Pageを作成

---
+++ app/controllers/checkouts_controller.rb
@@ -0,0 +1,14 @@
+class CheckoutsController <  ApplicationController
+  before_action :require_nonempty_card!
+  def show
+  end
+
+  def create
+  end
+
+  private
+
+  def require_nonempty_card!
+    redirect_to cart_path if current_cart.order_items.none?
+  end
+endt
--- app/models/order_item.rb
+++ app/models/order_item.rb
@@ -2,7 +2,16 @@ class OrderItem < ApplicationRecord
   belongs_to :order
   belongs_to :product
 
+  validates :quantity, numericality: {greater_than_or_equal_to: 0 }
+  after_save :destroy_if_zero
+
   def total
     quantity * product.amount
+  end
+
+  private
+
+  def destroy_if_zero
+    destroy if quantity.zero?
   end
 end
